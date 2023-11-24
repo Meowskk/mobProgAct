64809f69842b8b4c1f776c3d85c7ddb4484bba1e
meow <stephen.longaquit@gmail.com>
2023-11-24 22:36:14 +0800

    moew1

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import AddItem from "./components/AddItem";
+import ListItems from "./components/ListItems";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StyleSheet, Text, View } from "react-native";
 export default function App() {
+  const [list, setList] = useState([]);
+  const addItem = (text) => {
+    const newItem = {
+      id: uuidv4(),
+      task: text,
+    };
+    setList([newItem, ...list]);
+  };
+  const DeleteItem = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+        <View style={styles.container}></View>
+        <View style={styles.container}>
+          <Text>Todo List</Text>
+          <AddItem addItem={addItem}></AddItem>
+          <ListItems deleteItem={DeleteItem} listItems={list}></ListItems>
+          <StatusBar style="auto" />
+        </View>
+      </View>
+    </SafeAreaProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+/**module.exports = {
+  content: ["./app.js","./src/**//**.{}jsx,tsx,js,ts"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+*/
